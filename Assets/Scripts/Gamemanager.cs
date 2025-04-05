6ce72e126fb8db74856a4ffccfe03c7f94ad1a82
ViliusKk <vilius.kuokstas@gmail.com>
2025-04-05 18:23:34 +0300

    Spawning fruits

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    public GameObject[] fruits;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.y = 3;
+        Vector3 offset = new Vector3(0, 0, 10);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fruits[Random.Range(0, 3)], mousePos + offset, Quaternion.identity);
+        }
+    }
+}
